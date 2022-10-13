4fa7b5f4b86740e34df109cc2fa9156615ad412f
mo01012 <115111548+mo01012@users.noreply.github.com>
2022-10-13 12:01:16 +0200

    add page

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,9 +34,73 @@ $(document).ready(function() {
             }
         }
     })
-    $(".page-link").click(function(e) {
 
-
-        console.log($('this').text())
+    $('.owl-carousel').owlCarousel({
+        rtl: true,
+        margin: 10,
+        nav: true,
+        loop: true,
+        dots: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
     })
+
+    $num = $('.my-card').length;
+    $even = $num / 2;
+    $odd = ($num + 1) / 2;
+
+    if ($num % 2 == 0) {
+        $('.my-card:nth-child(' + $even + ')').addClass('active');
+        $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
+        $('.my-card:nth-child(' + $even + ')').next().addClass('next');
+    } else {
+        $('.my-card:nth-child(' + $odd + ')').addClass('active');
+        $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
+        $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
+    }
+
+    $('.my-card').click(function() {
+        $slide = $('.active').width();
+        console.log($('.active').position().left);
+
+        if ($(this).hasClass('next')) {
+            $('.card-carousel').stop(false, true).animate({ left: '-=' + $slide });
+        } else if ($(this).hasClass('prev')) {
+            $('.card-carousel').stop(false, true).animate({ left: '+=' + $slide });
+        }
+
+        $(this).removeClass('prev next');
+        $(this).siblings().removeClass('prev active next');
+
+        $(this).addClass('active');
+        $(this).prev().addClass('prev');
+        $(this).next().addClass('next');
+    });
+
+
+    // Keyboard nav
+    $('html body').keydown(function(e) {
+        if (e.keyCode == 37) { // left
+            $('.active').prev().trigger('click');
+        } else if (e.keyCode == 39) { // right
+            $('.active').next().trigger('click');
+        }
+    });
+    $(".right").click(function() {
+        $('.active').next().trigger('click');
+    })
+    $(".left").click(function() {
+        $('.active').prev().trigger('click');
+    })
+
 });
